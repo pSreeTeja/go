7f944eeda4a42a24d25bdbfc83e6a288ca715921
unknown <psteja02@gmail.com>
2024-11-21 23:12:05 +0530

    HelloWorld Go Prog

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,7 @@
-package main
+package main //needs to have a package ceratinly
 
-import "fmt"
+import "fmt" //"Format" package built-in Go
 
-func main() {
+func main() { //main method is necessary to start the program
 	fmt.Println("Welcome to our ToDo Application")
 }
